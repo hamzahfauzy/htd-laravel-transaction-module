7c0a5ecbe8d07b008ab37f21b1127a297379cbc8
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-07-15 16:49:21 +0700

    add auth middleware

--- Routes/web.php
+++ Routes/web.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('web')->group(function(){
     Route::match(['get','post'], 'transaction/bulk-create', [TransactionController::class, 'bulkCreate'])->name('transaction-bulk-create');
 
-    Route::get('reports/daily-transaction/detail/{date}', [TransactionController::class, 'dailyDetail'])->name('reports.reports/daily-transaction.detail');
-    Route::get('reports/monthly-transaction/detail/{date}', [TransactionController::class, 'detail'])->name('reports.reports/monthly-transaction.detail');
+    Route::middleware('auth')->group(function(){
+        Route::get('reports/daily-transaction/detail/{date}', [TransactionController::class, 'dailyDetail'])->name('reports.reports/daily-transaction.detail');
+        Route::get('reports/monthly-transaction/detail/{date}', [TransactionController::class, 'detail'])->name('reports.reports/monthly-transaction.detail');
+    });
 });
